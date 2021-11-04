9b8402492d2a66cf4617f4346d84422b025c87e9
Ogeday <ogeday@rubiklabs.com>
2021-11-05 02:42:55 +0300

    Added push-dir package

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/tmtb-qr-example/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'tmtb-qr-example',
